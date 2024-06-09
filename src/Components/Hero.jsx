56e06740f70e2f8b27b89015f825e939b9b50b27
biancavictorino <biancamarie.victorino@gmail.com>
2024-06-09 23:03:29 +0800

    fixed navbar menu

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,22 +5,22 @@ import {Stats} from '../Data/Statistics'
 
 const Hero = () => {
   return (
-    <div className='py-10 flex flex-col-reverse lg:flex-row lg:px-20'>
+    <div id='home' className='py-10 flex flex-col-reverse lg:flex-row lg:px-20'>
         <div className='flex flex-col items-center md:w-3/5 lg:items-start'>
             <h1 className='text-primary-color font-bold py-5 text-3xl text-center lg:text-5xl lg:text-start'>Discover your home away from home at Silong.</h1>
             <h2 className='text-text-color text-md text-center lg:text-start'>After a thrilling day on the water,  relax in our comfortable accommodations and enjoy the tranquility of Palawan.</h2>
             <div className='py-6'>
                 <PrimaryButton
                     btnText={'Take a Tour'}
-                    styles={'text-secondary-color bg-primary-color'}
+                    styles={'bg-primary-color text-secondary-color'}
                 />
             </div>
             <div className='flex flex-row justify-between py-8'>
                 {
                     Stats.map(data => (
-                        <div className='flex flex-row items-center'>
-                            <h1 className='text-primary-color font-bold text-xl pl-2 lg:text-3xl lg:pl-0'>{data.total}</h1>
-                            <p className='font-light text-text-color border-r-2 border-primary-color pr-2 text-sm lg:w-2/4'>{data.description}</p>
+                        <div className='flex flex-row'>
+                            <h1 className='text-primary-color text-xl px-1 font-semibold md:text-2xl md:px-2 lg:px-2'>{data.total}</h1>
+                            <p className='text-text-color border-r-2 border-primary-color md:w-3/5'>{data.description}</p>
                         </div>
                     ))
                 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,21 +4,39 @@ import PrimaryButton from './PrimaryButton'
 import {AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
-    // const [menuOpen, setMenuOpen] = useState(flase);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleOpen = () => {
+      setMenuOpen(!menuOpen);
+    }
 
   return (
     <div className='header py-6 flex flex-row justify-between items-center md:px-20'>
         <div className='logo'>
             <img src={PrimaryLogo} alt="Silong" className="w-12 h-12" />
         </div>
-        <div className='hidden text-base gap-2 w-auto'> 
-            <a href="#" className='p-4 text-text-color' >Home</a>
+        <div className='hidden text-base gap-2 w-auto md:flex'> 
+            <a href="#home" className='p-4 text-text-color' >Home</a>
             <a href="#" className='p-4 text-text-color'>Rooms</a>
             <a href="#" className='p-4 text-text-color'>Events</a>
             <a href="#" className='p-4 text-text-color'>Cuisine</a>
         </div>
-        {/* <PrimaryButton btnText={'Book Now'}/> */}
-        <AiOutlineMenu className='text-primary-color w-6 h-6' />
+
+        <div className='hidden md:flex'>
+          <PrimaryButton btnText={'Book Now'}/>
+        </div>
+
+        <AiOutlineMenu onClick={handleOpen} className='text-primary-color w-6 h-6 md:hidden' />
+        
+        <div className={`${menuOpen ? 'bg-secondary-color fixed h-screen right-0 top-0 w-full z-40 flex flex-col justify-center items-center gap-10' : 'hidden'}`}>
+          <svg onClick={handleOpen} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='text-text-color h-6 w-6 absolute top-9 right-8'>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+          <a href="#home" className='text-text-color text-base'>Home</a>
+          <a href="#" className='text-text-color text-base'>Rooms</a>
+          <a href="#" className='text-text-color text-base'>Events</a>
+          <a href="#" className='text-text-color text-base'>Cuisine</a>
+      </div>
     </div>
 
   )
